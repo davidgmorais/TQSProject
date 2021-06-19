ae16c689239d08ff6c8aa8ec4f5fbc92ad0b597c
Mariana Ladeiro <marianaladeiro@ua.pt>
2021-06-19 16:01:10 +0100

    engine controller documented

--- engine/src/main/java/com/example/engine/controller/EngineController.java
+++ engine/src/main/java/com/example/engine/controller/EngineController.java
@@ -8,6 +8,9 @@ import com.example.engine.dto.UserDTO;
 import com.example.engine.entity.Contrib;
 import com.example.engine.entity.OrderStatus;
 import com.example.engine.entity.Rider;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.SwaggerDefinition;
+import io.swagger.annotations.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +24,9 @@ import java.util.Map;
 
 
 @Controller
+@SwaggerDefinition(tags = {
+        @Tag(name = "Web Engine Controller", description = "Maps all the views and content needed for said views.")
+})
 public class EngineController {
     public static final String INDEX_PAGE = "redirect:/admin";
     public static final String LOGIN_PAGE = "redirect:/login";
@@ -43,13 +49,15 @@ public class EngineController {
 
     @Autowired
     JwtUtils jwtUtils;
-    
+
     @Autowired
     OrderController orderController;
 
 
     private static final Logger logger = LoggerFactory.getLogger(EngineController.class);
 
+
+    @ApiOperation(value = "Admin's front page showing riders and contributors requests as well as other information.", response = String.class)
     @GetMapping("/admin")
     public String index(Model model, UserDTO userDTO) {
         model.addAttribute(SEARCH_MODEL, userDTO);
@@ -71,30 +79,35 @@ public class EngineController {
         return "index";
     }
 
+    @ApiOperation(value = "Endpoint for admin to deny a contributor's request by its id to join the Engine.", response = String.class)
     @PostMapping("/deny/contrib/{id}")
     public String denyContrib(@PathVariable int id) {
         contribController.denyContrib(id);
         return INDEX_PAGE;
     }
 
+    @ApiOperation(value = "Endpoint for admin to deny a rider's request by its id to join the Engine.", response = String.class)
     @PostMapping("/deny/rider/{id}")
     public String denyRider(@PathVariable int id) {
         riderController.denyRider(id);
         return INDEX_PAGE;
     }
 
+    @ApiOperation(value = "Endpoint for admin to verify a contributor's request by its id to join the Engine.", response = String.class)
     @PostMapping("/verify/contrib/{id}")
     public String verifyContrib(@PathVariable int id) {
         contribController.verifyContrib(id);
         return INDEX_PAGE;
     }
 
+    @ApiOperation(value = "Endpoint for admin to verify a rider's request by its id to join the Engine.", response = String.class)
     @PostMapping("/verify/rider/{id}")
     public String verifyRider(@PathVariable int id) {
         riderController.verifyRider(id);
         return INDEX_PAGE;
     }
 
+    @ApiOperation(value = "Endpoint to search for a contributor or rider, already verified, by their username, and map the corresponding view.", response = String.class)
     @GetMapping("/search")
     public String searchPage(Model model,  UserDTO userDTO) {
         List<Contrib> resultsContrib = contribController.listAllContributors(userDTO.getUsername(), null);
@@ -105,13 +118,14 @@ public class EngineController {
         return "searchPage";
     }
 
-
+    @ApiOperation(value = "View of the login page", response = String.class)
     @GetMapping(value = "/login")
     public String login(Model model, UserDTO userDTO) {
         model.addAttribute("authenticateUser", userDTO);
         return "login";
     }
 
+    @ApiOperation(value = "Map the corresponding view, according to user role, after processing login.", response = String.class)
     @PostMapping(value = "/login")
     public String signIn(UserDTO userDTO, Model model) {
         var authorizationKey = "Authorization";
@@ -147,18 +161,20 @@ public class EngineController {
 
     }
 
-
+    @ApiOperation(value = "View of the signup page.", response = String.class)
     @GetMapping(value = "/signup")
     public String signup() {
         return SIGNUP_ERROR;
     }
 
+    @ApiOperation(value = "View of the signup page for contributors.", response = String.class)
     @GetMapping("/signup/service")
     public String signupForService(Model model, ContribDTO contribDTO){
         model.addAttribute("service", contribDTO);
         return "signupService";
     }
 
+    @ApiOperation(value = "Map the correct page after signup, according to success in signing up or error for contributors.", response = String.class)
     @PostMapping("signup/service")
     public String signupForService(ContribDTO contribDTO, Model model){
         ResponseEntity<String> registration = userController.registerContributor(contribDTO);
@@ -172,12 +188,14 @@ public class EngineController {
 
     }
 
+    @ApiOperation(value = "View of the signup page for riders.", response = String.class)
     @GetMapping("/signup/rider")
     public String signupRider(Model model, UserDTO rider){
         model.addAttribute("rider", rider);
         return "signupRider";
     }
 
+    @ApiOperation(value = "Map the correct page after signup, according to success in signing up or error for riders.", response = String.class)
     @PostMapping("/signup/rider")
     public String signupRider(UserDTO rider, Model model){
         ResponseEntity<String> registration = userController.registerRider(rider);
@@ -191,16 +209,19 @@ public class EngineController {
 
     }
 
+    @ApiOperation(value = "View of the front page for contributors.", response = String.class)
     @GetMapping(value = "/service/dashboard")
     public String service() {
         return INDEX_SERVICE;
     }
 
+    @ApiOperation(value = "View of the statistics page for contributors.", response = String.class)
     @GetMapping(value = "/service/statistics")
     public String serviceStatistics() {
         return "serviceStatistics";
     }
 
+    @ApiOperation(value = "View of the services page for admin, where requests and list of contributors are present.", response = String.class)
     @GetMapping(value = "/services")
     public String servicesPage(Model model, UserDTO userDTO) {
         model.addAttribute(SEARCH_MODEL, userDTO);
@@ -211,6 +232,7 @@ public class EngineController {
         return "servicesPage";
     }
 
+    @ApiOperation(value = "View of the riders page for admin, where requests and list of riders are present.", response = String.class)
     @GetMapping(value = "/riders")
     public String ridersPage(Model model, UserDTO userDTO) {
         model.addAttribute(SEARCH_MODEL, userDTO);
@@ -221,7 +243,7 @@ public class EngineController {
         return "ridersPage";
     }
 
-
+    @ApiOperation(value = "View of the front page for riders", response = String.class)
     @GetMapping(value = "/rider/dashboard")
     public String riderIndex(Model model) {
         model.addAttribute("status", status);
@@ -233,6 +255,7 @@ public class EngineController {
         return INDEX_RIDER;
     }
 
+    @ApiOperation(value = "Start shift as a rider.", response = String.class)
     @PostMapping(value = "/rider/dashboard/{log}/{lat}")
     public String startShiftRider(@PathVariable Double log, @PathVariable Double lat, Model model) {
         LocationDTO location = new LocationDTO(lat, log);
@@ -243,6 +266,7 @@ public class EngineController {
         return RIDER_DASHBOARD;
     }
 
+    @ApiOperation(value = "End shift as a rider.", response = String.class)
     @PostMapping(value = "/rider/dashboard/end")
     public String endShiftRider(Model model) {
         jwt = this.trimToken(jwt);
@@ -251,6 +275,7 @@ public class EngineController {
         return RIDER_DASHBOARD;
     }
 
+    @ApiOperation(value = "Update status of delivery for riders", response = String.class)
     @PostMapping(value = "/update/order/{status}")
     public String updateOrderStatus(@PathVariable String status) {
         jwt = this.trimToken(jwt);
@@ -270,6 +295,7 @@ public class EngineController {
         return RIDER_DASHBOARD;
     }
 
+    @ApiOperation(value = "View for rider's rating", response = String.class)
     @GetMapping(value = "/rider/rating")
     public String riderRating() {
         return "riderRating";
